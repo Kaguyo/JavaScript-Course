3bcbd0a93da6aa1f91e85b2bf17694ae7def9ae5
Gabriel <gabrieltroop27@gmail.com>
2024-09-14 19:24:34 -0300

    # procura id, dot procura class

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-let username;
-let password;
 
-document.getElementById("logInButton").onclick = function (){
-    username = document.getElementById("myText1").value;
-    password = document.getElementById("myText2").value;
-    document.getElementById("h1").textContent = `Hello ${username}`;
-}
+
