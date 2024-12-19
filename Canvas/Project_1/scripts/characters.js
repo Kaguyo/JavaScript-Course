15abfef431b66d919e9aa1af1947703d5b284001
Gabriel <gabrieltroop27@gmail.com>
2024-12-19 18:39:39 -0300

    Fixed auto setting stats algorithm

--- Canvas/Project_1/scripts/characters.js
+++ Canvas/Project_1/scripts/characters.js
@@ -23,12 +23,12 @@ class Characters {
         if (this.Id == 1 && this.Name == "Seele"){
             for (i; i < this.Level; i++){
                 this._HpMax += (30 * (1 + (countDecimal / 1.25)));
-                if (justifyStats.includes(this.Level)) countDecimal++;  
+                if (justifyStats.includes(i)) countDecimal++;  
             }
         } else if (this.Id == 2 && this.Name == "Keqing"){
             for (i; i < this.Level; i++){
                 this._HpMax += (32 * (1 + (countDecimal / 1.25)));
-                if (justifyStats.includes(this.Level)) countDecimal++;
+                if (justifyStats.includes(i)) countDecimal++;
             }
         }
         
@@ -41,12 +41,12 @@ class Characters {
         if (this.Id == 1 && this.Name == "Seele"){
             for (i = 0; i < this.Level; i++){
                 this._AtkMax += (9.2 * (1 + (countDecimal / 1.25)));
-                if (justifyStats.includes(this.Level)) countDecimal++;  
+                if (justifyStats.includes(i)) countDecimal++;  
             }
         } else if (this.Id == 2 && this.Name == "Keqing"){
             for (i = 0; i < this.Level; i++){
                 this._AtkMax += (8.6 * (1 + (countDecimal / 1.15)));
-                if (justifyStats.includes(this.Level)) countDecimal++;
+                if (justifyStats.includes(i)) countDecimal++;
             }
         }
 
@@ -59,12 +59,12 @@ class Characters {
         if (this.Id == 1 && this.Name == "Seele"){
             for (i = 0; i < this.Level; i++){
                 this._DefMax += (13.2 * (1 + (countDecimal / 1.25)));
-                if (justifyStats.includes(this.Level)) countDecimal++;  
+                if (justifyStats.includes(i)) countDecimal++;  
             }
         } else if (this.Id == 2 && this.Name == "Keqing"){
             for (i = 0; i < this.Level; i++){
                 this._DefMax += (12.6 * (1 + (countDecimal / 1.25)));
-                if (justifyStats.includes(this.Level)) countDecimal++;
+                if (justifyStats.includes(i)) countDecimal++;
             }
         }
 
@@ -74,7 +74,7 @@ class Characters {
 let justifyStats = [10,20,30,40,50,53,60,64,70,75,80,85,90,95,100,101,102,103,104,105];
 
 const Seele = new Characters(1, "Seele", 23, "1.75 m", "63 kg", "Female", {}, [], 79);
-const Keqing = new Characters(2, "Keqing", 19, "1.67 m", "58 kg", "Female", {}, [], 55)
+const Keqing = new Characters(2, "Keqing", 19, "1.67 m", "58 kg", "Female", {}, [], 79)
 
 console.log(Keqing);
 console.log(Seele);
